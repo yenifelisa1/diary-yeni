a609a80535c64fc32185b3a22c4f603adf9768ef
yenifelisa1 <felisayeni@gmail.com>
2023-03-07 15:10:29 +0700

    First Commit

---
+++ app.py
@@ -0,0 +1,68 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+from datetime import datetime
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/diary', methods=['GET'])
+def show_diary():
+    articles = list(db.diary.find({}, {'_id': False}))
+    return jsonify({'articles': articles})
+
+
+@app.route('/diary', methods=['POST'])
+def save_diary():
+    # sample[_receive = request.form['sample_give']
+    # printsample_receive]
+    title_receive = request.form.get("title_give")
+    content_receive = request.form.get("content_give")
+
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+    now = today.strftime('%Y.%m.%d')
+
+    file = request.files['file_give']
+    extension = file.filename.split('.')[-1]
+    filename = f'file-{mytime}.{extension}'
+    save_to = f'static/{filename}'
+    file.save(save_to)
+
+    profile = request.files['profile_give']
+    extension = profile.filename.split('.')[-1]
+    profilename = f'profile-{mytime}.{extension}'
+    save = f'static/{profilename}'
+    profile.save(save)
+
+    time = today.strftime('%Y.%m.%d')
+
+    doc = {
+        'file' : filename,    
+        'profile' : profilename,
+        'title': title_receive,
+        'content': content_receive,
+        'timenow': now
+    }
+    db.diary.insert_one(doc)
+    return jsonify({'msg': 'POST request complete!'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
